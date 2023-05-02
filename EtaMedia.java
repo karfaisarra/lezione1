3ade6739693f56938adf414601def44fc168be18
karfaisarra <karfaisarra@gmail.com>
2023-05-02 15:32:27 +0200

    add exercice average age

---
+++ EtaMedia.java
@@ -0,0 +1,9 @@
+package lezione1;
+
+public class EtaMedia {
+	public static void main(String args[]) {
+		double eta1 = 23, eta2 = 30, eta3 = 40;
+		double etaMedia = (eta1 +eta2 + eta3)/3;
+		System.out.println("l'eta media è di "+etaMedia);
+	}
+}
